8b2dbae5f831272cbf3801fe9cd9d041f7d182c5
Christopher Belcher <cpbelcher1@gmail.com>
2025-04-11 21:59:43 -0600

    Moved relevant variables inside of function to enable continuous time update/display

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-const bDay = new Date("2025-08-29T00:00:00");
-const now = new Date();
-
-const vBday = bDay.getTime();
-const vNow = now.getTime();
-const timeGap = (vBday - vNow)/(1000);
-
-const d = (timeGap/86400);
-const h = ((d % 1) * (24));
-const m = ((h % 1) * (60));
-const s = ((m % 1) * (60));
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
 
 // console.log(vBday);
 // console.log(vNow);
@@ -19,10 +12,16 @@ const s = ((m % 1) * (60));
 // console.log(s);
 
 function displayTime(){
-    const days = document.getElementById("days");
-    const hours = document.getElementById("hours");
-    const minutes = document.getElementById("minutes");
-    const seconds = document.getElementById("seconds");
+    const bDay = new Date("2025-08-29T00:00:00");
+    const now = new Date();
+    const vBday = bDay.getTime();
+    const vNow = now.getTime();
+    const timeGap = (vBday - vNow)/(1000);
+
+    const d = (timeGap/86400);
+    const h = ((d % 1) * (24));
+    const m = ((h % 1) * (60));
+    const s = ((m % 1) * (60));
 
     days.innerHTML = Math.floor(d);
     hours.innerHTML = Math.floor(h);
